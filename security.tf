5eb9fb9148c6bb0a1ee5923b668d03a41261acea
Prashanth <141617601+prashanthbandari123@users.noreply.github.com>
2026-02-06 11:57:25 +0530

    Add security groups for ALB and private EC2

---
+++ security.tf
@@ -0,0 +1,63 @@
+# Security Group for Load Balancer
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP access from internet"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# Security Group for EC2 (Private)
+resource "aws_security_group" "ec2_sg" {
+  name        = "private-ec2-sg"
+  description = "Allow traffic only from ALB and SSH from my IP"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow Strapi traffic ONLY from ALB
+  ingress {
+    description     = "Strapi from ALB"
+    from_port       = 1337
+    to_port         = 1337
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  # SSH access (replace with your IP)
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["49.43.235.210"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private-ec2-sg"
+  }
+}
